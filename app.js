4dd106e43c9634b47df9376747ea298584dc89ce
sanidhya70 <ksanidhya007@gmail.com>
2018-11-19 15:30:53 -0500

    Integrated MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var AssistantV1 = require('watson-developer-cloud/assistant/v1');
 var bodyParser = require('body-parser')
 const express= require('express')
+var mongoose= require('mongoose');
+
 const app=express()
 //use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+mongoose.connect('mongodb://127.0.0.1:27017/booking_db', { useNewUrlParser: true })
 var assistant = new AssistantV1({
   username: 'apikey',
   password: 'eHZn9TIsARfnFkF-hOVmUihbjFPmoxMLCjj_QljaHv5I',
   url: 'https://gateway-wdc.watsonplatform.net/assistant/api',
   version: '2018-02-16'
 });
+
+var user= mongoose.model('customers',{Name:String,
+                            Phone_number: String,
+                            Date: String,
+                            Time: String});
+
+new user ({"Name": "Sam Smith", "Phone_number": "3222333333", "Date": "11/23/2018","Time": "9.30"}).save(function( err,customers){
+
+})
+app.get('/test',(req,res)=>{
+    user.find({"Date": "11/23/2018","Time": "9.30"},function(err,customers){
+        res.send(customers)
+    })
+})
+
 var context ={}
 
 app.post('/message',(req,res)=>{
